f710ffe993a49b06061809a5ff1ec7ec3bdb0c3d
Pedro Bernardes <pedroabernardes11@gmail.com>
2022-11-17 13:12:58 -0300

    feat: create button process edit/create

--- src/pages/Content/buttonProcessScript.tsx
+++ src/pages/Content/buttonProcessScript.tsx
@@ -4,6 +4,8 @@ import AlertDialog from '../../components/Dialog';
 import '../../styles/global.css';
 import renderForm from './dialogScript';
 import IconButton from '@mui/material/IconButton';
+import { matchRows } from '../../services/utils';
+
 
 function sleep(delay = 0) {
     return new Promise((resolve) => {
@@ -20,18 +22,51 @@ const addStylesheet = () => {
     document.head.appendChild(robotoFont);
 };
 
-const addButtons = () => {
+const addButtons = async () => {
+
+    const processNumber = (document.getElementById('ifrArvore')as HTMLIFrameElement).contentWindow.document.getElementById('topmenu').getElementsByTagName('span')[0].innerHTML.substring(0,20);
+    const idProcedimento = (document.getElementById('ifrArvore')as HTMLIFrameElement).contentWindow.document.getElementById('topmenu').getElementsByTagName('span')[0].id.substring(4);
+
+    const reidiProcessesNumber = await matchRows([processNumber]);
+
     const divComandos = (document.getElementById('ifrVisualizacao')as HTMLIFrameElement).contentWindow.document.getElementById('divArvoreAcoes');
     const newDocButton = document.createElement('a');
+    newDocButton.setAttribute("class", "buttonProcess")
+
+    if(divComandos.getElementsByClassName("buttonProcess").length != 0){
+      const buttonList = divComandos.getElementsByClassName("buttonProcess")
+      for(let i = 0; i <= buttonList.length; i++){
+        divComandos.removeChild(buttonList[i])
+      }
+    }
     newDocButton.href = '#';
     newDocButton.tabIndex = 451;
     newDocButton.classList.add('botaoSEI');
-    newDocButton.innerHTML = `<img src="imagens/sei_incluir_documento.gif" class="infraCorBarraSistema" title="Adicionar novo documento">`;
-    const container = document.body.appendChild(document.createElement('div'));
-    newDocButton.addEventListener('click', () =>
-        render(<AlertDialog container={container} />, container)
+
+    if(reidiProcessesNumber[0] != undefined){
+        newDocButton.innerHTML = `<img src="imagens/sei_assinar.gif" class="infraCorBarraSistema" title="Editar controle ">`;
+        newDocButton.addEventListener('click', () =>
+        chrome.runtime.sendMessage({
+            action: 'showREIDIDialog',
+            type: 'edit',
+            idProcedimento: idProcedimento,
+            numeroProcesso: processNumber,
+          })
+    );
+    }
+    else{
+        newDocButton.innerHTML = `<img src="imagens/sei_documento_gerar_multiplo.gif" class="infraCorBarraSistema" title="Adicionar controle ">`;
+        newDocButton.addEventListener('click', () =>
+        chrome.runtime.sendMessage({
+            action: 'showREIDIDialog',
+            type: 'create',
+            idProcedimento: idProcedimento,
+            numeroProcesso: processNumber,
+          })
     );
 
+    }
+    
     divComandos.append(newDocButton);
 };
 
@@ -43,3 +78,9 @@ const buttonsScript = async () => {
 };
 
 buttonsScript();
+
+  chrome.runtime.onMessage.addListener((request) => {
+    if (request.action === 'reloadREIDI') {
+      addButtons();
+    }
+  });
